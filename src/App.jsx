27d0861c8eb632478da81fa668d56cb2f74cc5e6
Gargenziano03 <gargenziano03@gmail.com>
2024-11-27 15:00:54 +0100

    add variable apiUrl

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,27 @@ import HomePage from './pages/Homepage'
 import ChiSiamo from './pages/ChiSiamo'
 import ListPost from './pages/ListPost'
 import PostPage from './pages/PostPage'
+import GlobalContexts from './contexts/GlobalContexts'
+
+const apiUrl = 'http://127.0.0.1:3000'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/ChiSiamo' element={<ChiSiamo />} />
-            <Route path='/ListPost' element={<ListPost />} />
-            <Route path='/ListPost/:id' element={<PostPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalContexts.Provider value={{ apiUrl }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+
+              <Route path='/' element={<HomePage />} />
+              <Route path='/ChiSiamo' element={<ChiSiamo />} />
+              <Route path='/ListPost' element={<ListPost />} />
+              <Route path='/ListPost/:id' element={<PostPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContexts.Provider>
+
 
 
 
